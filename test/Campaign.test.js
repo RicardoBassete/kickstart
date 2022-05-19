37494a3fb7359fbdea45d877275d5b38e0b057a6
Ricardo Gonçalves Bassete <ricardogbassete@gmail.com>
2022-05-19 12:29:11 -0300

    Add more test scripts

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -48,4 +48,54 @@ describe('Campaigns', () => {
         const isContributor = await campaign.methods.approvers(accounts[1]).call()
         assert(isContributor)
     })
+
+    it('Requires a minimum contribution', async () => {
+        try {
+            await campaign.methods.contribute().send({
+                value: '0',
+                from: accounts[1]
+            })
+
+            assert.ok(false)
+        } catch (err) {
+            assert(err)
+        }
+        
+    })
+
+    it('Allows a manager to make a payment request', async () => {
+        await campaign.methods.createRequest('Buy batteries', '100', accounts[1]).send({
+            from: accounts[0],
+            gas: '1000000'
+        })
+        const request = await campaign.methods.requests(0).call()
+        assert.equal('Buy batteries', request.description)
+    })
+
+    it('Processes requests', async () => {
+        await campaign.methods.contribute().send({
+            from: accounts[0],
+            value: web3.utils.toWei('10', 'ether')
+        })
+
+        await campaign.methods.createRequest('Buy batteries', web3.utils.toWei('5', 'ether'), accounts[1]).send({
+            from: accounts[0],
+            gas: '1000000'
+        })
+
+        await campaign.methods.approveRequest(0).send({
+            from: accounts[0],
+            gas: '1000000'
+        })
+
+        await campaign.methods.finalizeRequest(0).send({
+            from: accounts[0],
+            gas: '1000000'
+        })
+
+        let balance = await web3.eth.getBalance(accounts[1])
+        balance = parseFloat(web3.utils.fromWei(balance, 'ether'))
+
+        assert(balance > 104)
+    })
 })
