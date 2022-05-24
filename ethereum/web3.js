0fdaca001c05558856824c09354f660e2ea3a0ff
Ricardo Gonçalves Bassete <ricardogbassete@gmail.com>
2022-05-24 11:27:19 -0300

    Update web3.js

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,7 +1,22 @@
 import Web3 from "web3";
- 
-window.ethereum.request({ method: "eth_requestAccounts" });
- 
-const web3 = new Web3(window.ethereum);
+import {infuraProjectId} from '../.env' 
+
+let web3
+
+if(typeof window !== 'undefined' && typeof window.ethereum !== 'undefined'){
+
+    // we are in the browser and metamask is running
+    window.ethereum.request({ method: "eth_requestAccounts" });
+
+    web3 = new Web3(window.ethereum);
+
+}else{
+
+    // we are on the server or the user is not running metamask
+    const provider = new Web3.providers.HttpProvider(infuraProjectId)
+    
+    web3 = new Web3(provider);
+
+}
  
 export default web3;
