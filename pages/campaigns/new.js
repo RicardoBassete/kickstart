c1b4f6417c839270c434f10addbecc5180a11d44
Ricardo Gonçalves Bassete <ricardogbassete@gmail.com>
2022-05-26 17:09:07 -0300

    Add loading animation

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -9,22 +9,26 @@ import web3 from '../../ethereum/web3'
 class CampaignNew extends Component {
   state = {
     minimumContribution: '',
-    errorMessage: ''
+    errorMessage: '',
+    loading: false
   }
 
   onSubmit = async event => {
     event.preventDefault()
 
+    this.setState({loading: true, errorMessage: ''})
+    
     try {  
       const accounts = await web3.eth.getAccounts()
       await factory.methods
-        .createCampaign(this.state.minimumContribution)
-        .send({
-          from: accounts[0]
-        })
+      .createCampaign(this.state.minimumContribution)
+      .send({
+        from: accounts[0]
+      })
     } catch (err) {
       this.setState({errorMessage: err.message})
     }
+    this.setState({loading: false})
   }
 
   render() {
@@ -42,7 +46,7 @@ class CampaignNew extends Component {
             />
           </Form.Field>
           <Message error header="Oops!" content={this.state.errorMessage} />
-          <Button primary>Create</Button>
+          <Button primary loading={this.state.loading}>Create</Button>
         </Form>
       </Layout>
     )
